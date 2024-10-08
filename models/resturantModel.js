4a0a0f7d76cabbe8ddb333b7b9b4156abc9922c1
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2024-10-08 17:24:24 +0300

    Create resturantModel.js

---
+++ models/resturantModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const resturantsSchema = new mongoose.Schema({
+
+    restaurantName: {
+        type: String,
+        required: [true, "Resturant Name is required"],
+    },
+    fullAddress: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+    },
+    openTime: {
+        type: Number,
+        required: true,
+    },
+    closeTime: {
+        type: Number,
+        required: true,
+    },
+
+});
+
+module.exports = mongoose.model('Resturant', resturantsSchema);
