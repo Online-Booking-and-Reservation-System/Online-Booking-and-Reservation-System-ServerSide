019ce52db4aed04c0b612087b8e39feb0dc76088
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2024-10-09 20:55:58 +0300

    Update resturantRouter.js

--- routes/resturantRouter.js
+++ routes/resturantRouter.js
@@ -1,15 +1,47 @@
 const express = require('express');
-const resturantController = require('../controller/resturantController');
 
 const router = express.Router();
 
-router.route('/').get(resturantController.getAllRestaurants).post(resturantController.createRestaurant);
+const multer = require('multer');
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads');
+    },
+    filename: function (req, file, cb) {
+        const ext = file.mimetype.split('/')[1];
+        const fileName = `user-${Date.now()}.${ext}`;
+        cb(null, fileName);
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    const imageType = file.mimetype.split('/')[0];
+
+    if (imageType === 'image') {
+        return cb(null, true)
+    } else {
+        return cb(appError.create('file must be an image', 400), false)
+    }
+}
+
+const upload = multer({
+    storage: diskStorage,
+    fileFilter
+})
+
+
+const resturantController = require('../controller/resturantController');
+
+
+router.route('/').get(resturantController.getAllRestaurants);
+router.route('/').post(upload.single('imgUrl'), resturantController.createRestaurant);
 
 router.route('/:id').get(resturantController.getResturant).patch(resturantController.updateResturant).delete(resturantController.deleteResturant);
 
 
 
-module.exports = router;
 
+module.exports = router;
 
 
