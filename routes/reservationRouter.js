87da2093cbe8a3562a4286dc433f70522532c8fd
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2024-10-14 17:40:05 +0300

    Update reservationRouter.js

--- routes/reservationRouter.js
+++ routes/reservationRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const reservationController = require('../controller/reservationController');
 
-router.post('/create', reservationController.createReservation);
+router.post('/', reservationController.createReservation);
 
 router.get('/', reservationController.getAllReservations);
 
@@ -10,10 +10,6 @@ router.route('/:id').patch(reservationController.updateReservation).delete(reser
 
 router.get('/resturant/:restaurantName', reservationController.getAllReservationsForRestaurant);
 
-// Route for creating a PayPal order
-router.post('/create-order', reservationController.createOrder);
 
-// Route for capturing the PayPal order
-router.post('/capture-order', reservationController.captureOrder);
 
 module.exports = router;
