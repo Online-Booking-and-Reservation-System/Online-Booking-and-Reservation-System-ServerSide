a4ae77fb348e939c794a161482e22c1a296b3580
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2024-10-16 15:14:05 +0300

    Update reservationRouter.js

--- routes/reservationRouter.js
+++ routes/reservationRouter.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const reservationController = require('../controller/reservationController');
 
 router.post('/', reservationController.createReservation);
+// Capture PayPal payment after approval
+router.post('/capture-payment', reservationController.capturePayment);
+
+// Handle payment cancellation
+router.get('/payment-cancel', reservationController.paymentCancel);
+
 
 router.get('/', reservationController.getAllReservations);
 
